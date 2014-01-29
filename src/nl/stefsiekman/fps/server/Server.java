f521c100608e08399dff2056ec003765a5dc9bb0
bartbart333 <bartholdbos@hotmail.com>
2014-01-29 20:39:14 +0100

    Server add loadProperties()

--- src/nl/stefsiekman/fps/server/Server.java
+++ src/nl/stefsiekman/fps/server/Server.java
@@ -1,5 +1,11 @@
 package nl.stefsiekman.fps.server;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import tk.bartbart333.logger.ConsoleHandler;
 import tk.bartbart333.logger.Handler;
 import tk.bartbart333.logger.Level;
@@ -8,6 +14,8 @@ import tk.bartbart333.logger.Logger;
 public class Server {
 	
 	public static final String version = "v0.01";
+	public static File serverfolder = new File("server");
+	public static Properties properties = new Properties();
 	
 	private boolean gui = false;
 	private Handler handler;
@@ -15,6 +23,14 @@ public class Server {
 	
 	public Server(){
 		init();
+		
+		try{
+			loadProperties();
+		}catch(FileNotFoundException e){
+			e.printStackTrace();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 	
 	private void init(){
@@ -30,6 +46,17 @@ public class Server {
 		Logger.log(Level.INFO, "Starting server " + version);
 		
 		begintime = getTime();
+	}
+	
+	private void loadProperties() throws FileNotFoundException, IOException{
+		File file = new File(serverfolder, "server.properties");
+		if(!file.exists()){
+			file.createNewFile();
+			Logger.log(Level.INFO, "Creating server.properties");
+		}
+		
+		properties.load(new FileInputStream(file));
+		Logger.log(Level.INFO, "Loaded server.properties");
 	}
 	
 	public long getTime(){
