ef96492dcb40a23869c34d7d8b431cc17c041d4b
bartbart333 <bartholdbos@hotmail.com>
2014-01-29 17:46:50 +0100

    Server add init()

--- src/nl/stefsiekman/fps/server/Server.java
+++ src/nl/stefsiekman/fps/server/Server.java
@@ -1,10 +1,35 @@
 package nl.stefsiekman.fps.server;
 
+import tk.bartbart333.logger.ConsoleHandler;
+import tk.bartbart333.logger.Handler;
+import tk.bartbart333.logger.Level;
+import tk.bartbart333.logger.Logger;
+
 public class Server {
 	
+	public static final String version = "v0.01";
+	
+	private boolean gui = false;
+	private Handler handler;
+	private long begintime;
 	
 	public Server(){
+		init();
+	}
+	
+	private void init(){
+		Logger.init();
 		
+		if(gui){
+			
+		}else{
+			handler = new ConsoleHandler();
+		}
+		
+		Logger.addHandler(handler);
+		Logger.log(Level.INFO, "Starting server " + version);
+		
+		begintime = getTime();
 	}
 	
 	public long getTime(){
